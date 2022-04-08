8fadbde7ae3a301181010be2db3848fde800d614
morsapaes <marta.paes.moreira@gmail.com>
2022-04-08 10:26:44 -0400

    Add test

---
+++ .github/tests/dbt-get-started.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# Tests the dbt-get-started demo.
+
+set -euxo pipefail
+
+# Turn on the demo and give it a few seconds to spin up.
+docker-compose up -d
+sleep 5
+
+# Run the dbt models
+docker-compose exec -T dbt dbt run
+
+# Check that there's data making it's way to the avg_bid materialized view
+record_count=$(docker-compose run -T mzcli -Atc 'SELECT COUNT(*) FROM avg_bid;')
+[[ "$record_count" -gt 0 ]]
