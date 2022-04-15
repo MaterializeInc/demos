fb452a2b62d38214dbe1ddefa3df28742c3eb1cd
Anna Glander <anna@materialize.com>
2022-04-15 11:22:44 -0400

    chbench: working compose.yml file

--- chbench/README.md
+++ chbench/README.md
@@ -5,12 +5,3 @@ TPC-H designed to test the speed of analytics queries on a rapidly changing
 dataset.
 
 Documentation: <https://materialize.com/docs/demos/business-intelligence/>
-
-TEMPORARY DEBUG/RUN Instructions:
-
-Start everything up, make sure things are all healthy, then:
-```
-docker-compose run chbench gen --config-file-path=/etc/chbenchmark/mz-default-mysql.cfg --warehouses=1
-curl -X POST -H "Content-Type: application/json" -d @mysql-connector.json http://localhost:8083/connectors
-docker-compose run chbench run --config-file-path=/etc/chbenchmark/mz-default-mysql.cfg --dsn=mysql --gen-dir=/var/lib/mysql-files --analytic-threads=0 --transactional-threads=1 --run-seconds=86400 --mz-sources
-```
--- chbench/mysql-connector.sh
+++ chbench/mysql-connector.sh
@@ -1,16 +1,21 @@
-{
-  "name": "mysql-connector",
-  "config": {
+#!/usr/bin/env bash
+
+echo "Deploying Debezium MySQL connector"
+
+curl -s -X PUT -H  "Content-Type:application/json" http://debezium:8083/connectors/register-mysql/config \
+    -d '{
     "connector.class": "io.debezium.connector.mysql.MySqlConnector",
     "database.hostname": "mysql",
-    "database.port": "3306",
+    "database.port": 3306,
     "database.user": "debezium",
     "database.password": "debezium",
     "database.server.name": "debezium",
-    "database.server.id": "1234",
+    "database.server.id": "223344",
+    "database.allowPublicKeyRetrieval": true,
     "database.history.kafka.bootstrap.servers": "kafka:9092",
     "database.history.kafka.topic": "mysql-history",
-    "database.allowPublicKeyRetrieval": "true",
+    "database.include.list": "tpcch",
     "time.precision.mode": "connect"
-  }
-}
+ }'
+
+sleep 1m
