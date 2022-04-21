71e454b4be2d0ce8d44a49dd527655925fd69c62
Joaquin Colacci <joaquincolacci@gmail.com>
2022-04-21 09:44:12 -0400

    Antennas Manhattan demo (#24)
    * Antennas Manhattan demo
    
    * Pre-commit changes. Added images + linting + refactors.
    
    * Pre-commit dockerfile fix
    
    * Moved folder place otherwise the test crash.
    
    * Permissions in shells.
    
    * Fix issues around cleaning cache
    
    * Little fix in the microservice
    
    * Update tests for the antennas projects
    
    * Reuse front-end for both projects

---
+++ .github/tests/antennas-kafka.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Tests the antennas Kafka demo.
+
+set -euxo pipefail
+
+# Install jq to parse JSON
+curl -fsSL http://stedolan.github.io/jq/download/linux64/jq > /usr/local/bin/jq
+chmod +x /usr/local/bin/jq
+
+# Turn on the demo and give it a few (good) seconds to spin up.
+docker-compose up -d
+sleep 70
+
+# Run a SQL using the sql endpoint
+count="$(curl -X POST 'localhost:6875/sql' -d 'sql=SELECT COUNT(1) FROM last_half_minute_updates' | jq -r '.results[0].rows[0][0]')"
+
+if [ "$count" -le 0 ];
+then
+    exit 5;
+fi
---
+++ antennas-kafka/backend/src/MaterializeClient/TransformStream/index.ts
@@ -0,0 +1,51 @@
+import {Transform} from 'stream';
+
+interface Antenna {
+  antenna_id: string;
+  geojson: string;
+  performance: number;
+}
+
+/**
+ * This class creates a batch of chunks. In this way every chunk is not a row but an array of rows.
+ * This will improve the performance of the writing.
+ * A timeout is needed in case the batch length is lower than the highwatermark for a long period of time.
+ */
+export default class TransformStream extends Transform {
+  batch = new Array<Antenna>();
+
+  size: number;
+
+  constructor() {
+    super({
+      highWaterMark: 100,
+      objectMode: true,
+    });
+
+    this.cleanBatch();
+  }
+
+  cleanBatch() {
+    this.batch = new Array<Antenna>();
+  }
+
+  _transform(row: any, encoding: string, callback: () => void) {
+    const {mz_progressed: mzProgressed} = row;
+
+    if (mzProgressed) {
+      this.push(this.batch);
+      this.cleanBatch();
+    } else {
+      this.batch.push(row);
+    }
+    callback();
+  }
+
+  _flush(callback: () => void) {
+    if (this.batch.length) {
+      this.push(this.batch);
+      this.cleanBatch();
+    }
+    callback();
+  }
+}
