10c171e99e346035cc01eecb510f912e9dc44462
Bobby Iliev <bobby@bobbyiliev.com>
2022-11-02 17:41:13 +0200

    Antennas postgres demo for MZ Cloud

--- antennas-postgres/helper/src/app.ts
+++ antennas-postgres/helper/src/app.ts
@@ -55,7 +55,7 @@ async function setUpMaterialize() {
     CREATE MATERIALIZED VIEW IF NOT EXISTS last_half_minute_performance_per_antenna AS
     SELECT A.antenna_id, A.geojson, AVG(AP.performance) as performance
     FROM antennas A JOIN antennas_performance AP ON (A.antenna_id = AP.antenna_id)
-    WHERE ((CAST(EXTRACT( epoch from AP.updated_at) AS NUMERIC) * 1000) + 30000) > mz_now()
+    WHERE (cast("updated_at" as timestamp) + INTERVAL '1 HOUR' ) > mz_now()
     GROUP BY A.antenna_id, A.geojson;
   `);
   }
