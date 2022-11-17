108366f90dd9f97973f141bb217803284823910a
morsapaes <marta.paes.moreira@gmail.com>
2022-11-17 13:14:23 +0100

    Remove Docker-specific checks and test workflows

--- .github/tools/config.py
+++
@@ -1,8 +0,0 @@
-"""Configuration shared by the tools in this directory."""
-
-from pathlib import Path
-
-WORKFLOW_PATH = Path(".github/workflows/main.yml")
-IMAGES_PATH = Path(".github/tools/images.yml")
-REQUIREMENTS_TXT_PATH = Path(".github/tools/requirements.txt")
-PRECOMMIT_CONFIG_PATH = Path(".pre-commit-config.yaml")
