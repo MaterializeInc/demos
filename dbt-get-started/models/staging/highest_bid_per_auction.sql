feb031b71805a04ea22675ca667373234bedd67d
Bobby Iliev <bobby@bobbyiliev.com>
2022-12-21 11:20:16 +0200

    SQLFluff suggested changes (#49)
    * SQLFluff suggested changes
    
    * Add SQLfluff steps

--- dbt-get-started/models/staging/highest_bid_per_auction.sql
+++ dbt-get-started/models/staging/highest_bid_per_auction.sql
@@ -1,10 +1,18 @@
 {{ config(materialized='view') }}
 
-SELECT grp.auction_id, bid_id, buyer_id, seller_id, item, amount, bid_time, end_time
+SELECT
+    grp.auction_id,
+    bid_id,
+    buyer_id,
+    seller_id,
+    item,
+    amount,
+    bid_time,
+    end_time
 FROM
-    (SELECT DISTINCT auction_id FROM {{ ref('on_time_bids') }}) grp,
-LATERAL (
-    SELECT * FROM {{ ref('on_time_bids') }}
-    WHERE auction_id = grp.auction_id
-    ORDER BY amount DESC LIMIT 1
-)
+    (SELECT DISTINCT auction_id FROM {{ ref('on_time_bids') }}) AS grp,
+    LATERAL(
+        SELECT * FROM {{ ref('on_time_bids') }}
+        WHERE auction_id = grp.auction_id
+        ORDER BY amount DESC LIMIT 1
+    )
