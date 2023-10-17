83fb6d6e9bc9214beb2bd04ee1b76e0a4424f508
Bobby Iliev <bobby@bobbyiliev.com>
2023-10-17 12:13:31 +0000

    Upgrade eCommerce demo to Debezium v2

--- ecommerce-redpanda/mysql/mysql_dbz.sh
+++ ecommerce-redpanda/mysql/mysql_dbz.sh
@@ -9,12 +9,14 @@ curl -s -X PUT -H  "Content-Type:application/json" http://debezium:8083/connecto
     "database.port": 3306,
     "database.user": "debezium",
     "database.password": "'"${MYSQL_PASSWORD}"'",
-    "database.server.name": "mysql",
     "database.server.id": "223344",
     "database.allowPublicKeyRetrieval": true,
     "database.history.kafka.bootstrap.servers":"'"$KAFKA_ADDR"'",
     "database.history.kafka.topic": "mysql-history",
+    "schema.history.internal.kafka.bootstrap.servers": "'"$KAFKA_ADDR"'",
+    "schema.history.internal.kafka.topic": "mysql-internal-history",
     "database.include.list": "shop",
+    "topic.prefix": "dbserver1",
     "time.precision.mode": "connect",
     "include.schema.changes": false
  }'
